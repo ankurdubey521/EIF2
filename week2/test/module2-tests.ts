97e6eecd9972b9f2b2c2025534922d5b78cbbbca
Ankur Dubey <ankurdubey521@protonmail.com>
2021-02-09 19:07:53 +0530

    week2: module 2

---
+++ week2/test/module2-tests.ts
@@ -0,0 +1,25 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { expect } from "chai";
+import { Contract, BigNumber } from "ethers";
+import { ethers } from "hardhat";
+
+describe("Storage Module 2d", function () {
+  let storageContract: Contract;
+
+  before(async function () {
+    const storage = await ethers.getContractFactory("StorageModule2");
+    storageContract = await storage.deploy();
+  });
+
+  it("Should store uints properly", async function () {
+    const randomNumbers = Array(100)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 1e6));
+    for (let value of randomNumbers) {
+      await storageContract.store(value);
+      await expect((await storageContract.retrieve()).toString()).equals(
+        value.toString()
+      );
+    }
+  });
+});
