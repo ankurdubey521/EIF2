603e323bade1d8bedd9f3deb7cab2c5bd36d55b1
Ankur Dubey <ankurdubey521@protonmail.com>
2021-02-10 20:21:49 +0530

    module5: dead man switch

---
+++ week2/scripts/module5/dead-man-check-alive.ts
@@ -0,0 +1,33 @@
+import fs, { readFileSync } from "fs";
+import path from "path";
+
+const deadManCheckStillAlive = async () => {
+  const address = "0xc9d1F72475FD6d6034399032704d7EAced7FDd28";
+  const [owner] = await ethers.getSigners();
+
+  const rawAbi = fs
+    .readFileSync(
+      path.resolve(
+        __dirname,
+        "../../artifacts/contracts/module5/DeadManSwitch.sol/DeadManSwitch.json"
+      )
+    )
+    .toString();
+  const abi = JSON.parse(rawAbi).abi;
+
+  const contract = new ethers.Contract(address, abi, owner);
+
+  console.log(
+    `Last alive called block: ${await contract.lastAliveCalledBlock()}`
+  );
+
+  const hash = (await await contract.checkIfStillAliveWasCalledRecently()).hash;
+  console.log(`checkIfStillAliveWasCalledRecently() tx hash: ${hash}`);
+};
+
+deadManCheckStillAlive()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
